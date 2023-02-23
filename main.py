1db4808c833dbdc5fb2e0fa9d385341c433c90c7
Artemii Dovzhenko <rockenny@ymail.com>
2023-02-23 10:51:30 +0300

    add methods to Question class add list of question objects

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from utils import get_questions
+from utils import get_questions, Question
 
 
 def main():
     print('Игра начинается!')
 
-    question_bank = get_questions()
+    question_bank: list[dict] = get_questions()
+    questions_list: list[Question] = [Question(i['q'], i['d'], i['a']) for i in question_bank]
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -10,7 +10,7 @@ def get_questions() -> list[dict]:
 
 
 class Question:
-    def __int__(self, text: str, difficulty: str, answer: str):
+    def __init__(self, text: str, difficulty: str, answer: str):
         self.text: str = text
         self.difficulty: int = int(difficulty)
         self.answer: str = answer
@@ -18,3 +18,35 @@ class Question:
         self.question_asked: bool = False
         self.user_answer: None = None
         self.question_value: int = self.difficulty * 10
+
+    def get_points(self) -> int:
+        """Возвращает int, количество баллов.
+        Баллы зависят от сложности: за 1 дается 10 баллов, за 5 дается 50 баллов.
+        """
+        return self.question_value
+
+    def _is_correct(self, user_input: str) -> bool:
+        """Возвращает True, если ответ пользователя совпадает
+        с верным ответов иначе False.
+        """
+        self.user_answer = user_input
+        return self.answer.lower() == self.user_answer.lower()
+
+    def build_question(self):
+        """Возвращает вопрос в понятном пользователю виде, например:
+        Вопрос: What do people often call American flag?
+        Сложность 4/5
+        """
+        self.question_asked = True
+        return f'Вопрос: {self.text}\nСложность {self.difficulty}/5'
+
+    def build_feedback(self, user_input: str):
+        """Возвращает :
+        Ответ верный, получено __ баллов,
+        Ответ неверный, верный ответ __
+        """
+        if self._is_correct(user_input):
+            # TODO: И куда мы блять складываем эти баллы?
+            return f"Ответ верный, получено {self.question_value} баллов"
+        else:
+            return f"Ответ неверный. Верный ответ – {self.answer}"
