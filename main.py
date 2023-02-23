d067d8bb10c3fdcd6a1f0048a4729eed64bcecd1
Artemii Dovzhenko <rockenny@ymail.com>
2023-02-23 11:17:12 +0300

    refactor creating question_list

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+import random
+
 from utils import get_questions, Question
+
+
+def create_questions_list(bank):
+    questions_list = [Question(i['q'], i['d'], i['a']) for i in bank]
+    random.shuffle(questions_list)
+    return questions_list
 
 
 def main():
     print('Игра начинается!')
 
     question_bank: list[dict] = get_questions()
-    questions_list: list[Question] = [Question(i['q'], i['d'], i['a']) for i in question_bank]
+    questions_list: list[Question] = create_questions_list(question_bank)
 
 
 if __name__ == '__main__':
