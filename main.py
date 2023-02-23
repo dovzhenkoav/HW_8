1c60d6c633840ce27f7f5c6394f819a51c38ec54
Artemii Dovzhenko <rockenny@ymail.com>
2023-02-23 10:11:05 +0300

    add get_questions func

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from utils import get_questions
+
+
 def main():
-    pass
+    print('Игра начинается!')
+
+    question_bank = get_questions()
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,2 +1,12 @@
+from settings import QUESTIONS_ENDPOINT
+import requests
+
+
+def get_questions() -> list[dict]:
+    """Берём с  эндпоинта JSON и возвращаем его в формате python"""
+    responce = requests.get(QUESTIONS_ENDPOINT)
+    return responce.json()
+
+
 class Question:
     pass
